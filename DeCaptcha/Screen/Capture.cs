59f7d28473244c6c169e84f13b867ff2ab0d10c7
julien.quartier <julien.quartier@bluewin.ch>
2017-01-29 21:56:52 +0100

    Capture

---
+++ DeCaptcha/Screen/Capture.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeCaptcha.Screen
+{
+    class Capture
+    {
+        Bitmap bmpScreenshot;
+        static Size gameZone = new Size(525, 90);
+        public short[,] anaGameZone = new short[gameZone.Width, gameZone.Height];
+
+        public Bitmap Screenshot()
+        {
+            bmpScreenshot = new Bitmap(gameZone.Width, gameZone.Height);
+
+            using (var g = Graphics.FromImage(bmpScreenshot))
+            {
+                g.CopyFromScreen(87, 162, 0, 0, Screen.PrimaryScreen.Bounds.Size);
+            }
+
+
+            return bmpScreenshot;
+        }
+    }
+}
